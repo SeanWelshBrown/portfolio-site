686af653efd1461c1dde1edf6b84493ef7b9b1a1
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-07-02 19:05:40 -0400

    Created 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function FourOhFour() {
+  return (
+    <div style={{ textAlign: `center` }}>
+      <br />
+      <h1>404</h1>
+      <br/>
+      <h1>This page doesn't exist. Bummer!</h1>
+    </div>
+  )
+}
