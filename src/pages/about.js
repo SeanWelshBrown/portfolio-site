c07daadacc4779abe9589b2da92b4468eb739ef0
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-07-05 08:29:37 -0400

    Updated some of the phrasing on the About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -43,13 +43,13 @@ export default function Home() {
 
         <p>I've had a passion for technology and all things nerdy since I was first old enough to hold a Game Boy and tinker with my father's MS-DOS work laptop, and that love of software and circuits has been the core of my identity ever since.</p>
 
-        <p>I've also maintained a love for the arts and humanities thanks to wonderful parents who took me to museums, plays, and everything in-between while growing up. I loved it so much that I acquired a <strong>BFA</strong> from <strong>NYU</strong> (with a <strong>Minor in Web Design and Applications</strong>), and have spent the past 10 years pursuing acting and voice-over professionally in NYC.</p>
+        <p>I've also maintained a love for the arts and humanities thanks to wonderful parents who took me to museums, plays, and everything in-between while growing up. I loved it so much that I acquired a <strong>BFA from NYU</strong> (with a <strong>Minor in Web Design and Applications</strong>), and have spent the past 10 years pursuing acting and voice-over professionally in NYC.</p>
 
         <p>As an actor and narrator I've worked with some amazing clients, such as <strong>Audible</strong>, <strong>Scholastic</strong>, <strong>Microsoft</strong>, <strong>HarperCollins</strong>, <strong>Avis</strong>, <strong>Nickelodeon</strong>, and many more.</p>
 
         {/* <p>My experiences in the performing arts have helped shape me in more ways than I could count, and have made <em>empathy</em>, <em>collaboration</em>, <em>compassion</em> and <em>diversity</em> into core apects of how I approach the world.</p> */}
 
-        <p>Once I knew I was ready for a change, I decided that it was time to pursue my other lifelong love of technology and software. I decided to attend <strong>Flatiron School</strong> for their <strong>Software Engineering</strong> program, spending an incredible 15 weeks learning everything I possibly could about modern web design and development.</p>
+        <p>Once I knew I was ready for a change, I decided that it was time to pursue my other lifelong love of technology and software. I attended <strong>Flatiron School</strong> for their <strong>Software Engineering</strong> program, spending an incredible 15 weeks learning everything I could about modern web design and development.</p>
 
         <p>I've discovered an even deeper passion for programming and software development than ever before, and am ready to chase my dream of bringing the arts, humanities, technology, and compassion together to make a difference in the world.</p>
 
