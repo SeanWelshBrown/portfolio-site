e9fae373f15a5d15f7ed2ca53f85b2ca06b9e292
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-07-16 10:02:40 -0400

    Updated Resume PDF

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout"
 import Content from "../components/content-div"
 
 // images
-import ResumePDF from "../images/sean-welsh-brown-software-engineer-resume.pdf"
+import ResumePDF from "../images/Sean_Welsh_Brown_RESUME.pdf"
 
 
 export default function Resume() {
@@ -16,7 +16,7 @@ export default function Resume() {
 
         <object data={ResumePDF} type="application/pdf" aria-label="Sean Welsh Brown Software Engineering Resume" style={{ height: `75vh`, width: `100%` }}></object>
 
-        <a href={ResumePDF} download="Sean Welsh Brown - Software Engineer - Resume">Download the PDF</a>
+        <a href={ResumePDF} download="Sean Welsh Brown - Software Engineer - Resume">Download as a PDF</a>
         
       </Content>
 
