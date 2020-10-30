9ce7617533d5aa764a7364a59ec8ccf34b2c17a8
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-10-30 09:56:24 -0400

    Updated Index page: changed current location, and updated skills.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,36 +8,57 @@ import ContactList from "../components/contact-list"
 // images
 import SeanHeadshot from "../images/sean/sean-headshot.jpg"
 
-
 export default function Home() {
   return (
-    
     <Layout>
-
       <Content>
-
-        <img 
-          src={SeanHeadshot} alt="professional headshot" 
-          style={{ 
-            borderRadius: `50%`, 
+        <img
+          src={SeanHeadshot}
+          alt="professional headshot"
+          style={{
+            borderRadius: `50%`,
             width: `300px`,
-            boxShadow: `0 0 8px 2px` }}
+            boxShadow: `0 0 8px 2px`,
+          }}
         />
 
-        <h2>Hi there, <span role="img" aria-label="Hand Waving Emoji">👋</span></h2>
+        <h2>
+          Hi there,{" "}
+          <span role="img" aria-label="Hand Waving Emoji">
+            👋
+          </span>
+        </h2>
 
-        <p>I'm Sean, a full-stack web developer and software engineer based in Brooklyn, NY.</p>
+        <p>
+          I'm Sean, a full-stack Web Developer and Software Engineer based in
+          New Jersey and New York City.
+        </p>
 
-        <p>I'm experienced in: <strong>JavaScript, React, Redux, Gatsby, Ruby, Rails, HTML and CSS</strong>, with an understanding of <strong>SQL, PostgreSQL, and RESTful</strong> database and API structures.</p>
+        <p>
+          I'm experienced in:{" "}
+          <b>JavaScript, React, Redux, Gatsby, Ruby on Rails, HTML and CSS</b>,
+          with an understanding of{" "}
+          <b>SQL, PostgreSQL, and RESTful database and API structures.</b>
+        </p>
 
-        <p>With a background in the performing arts, I'm driven by a lifelong passion to combine technology, software, humanity and art to create clean code and improve the lives of others.</p>
+        <p>
+          I'm currently working to add <b>Node.JS, Express, MongoDB</b> and
+          other new technologies to my skills.
+        </p>
 
-        <p>Click the links at the top of the page to learn more about me, to see my projects and resume, or to contact me directly!</p>
+        <p>
+          With a background in the performing arts, I'm driven by a lifelong
+          passion to combine technology, software, humanity and art to create
+          clean code and improve the lives of others.
+        </p>
+
+        <p>
+          Click the links at the top of the page to learn more about me, to see
+          my projects and resume, or to contact me directly!
+        </p>
 
         <ContactList />
-
       </Content>
-
     </Layout>
   )
 }
