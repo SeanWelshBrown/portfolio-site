23290a2053c77b483a5740603274e8892080ec02
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-10-30 10:17:16 -0400

    Updated Resume Page: reformatted using Prettier

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -7,19 +7,21 @@ import Content from "../components/content-div"
 // images
 import ResumePDF from "../images/Sean_Welsh_Brown_RESUME.pdf"
 
-
 export default function Resume() {
   return (
     <Layout>
-
       <Content>
+        <object
+          data={ResumePDF}
+          type="application/pdf"
+          aria-label="Sean Welsh Brown Software Engineering Resume"
+          style={{ height: `75vh`, width: `100%` }}
+        ></object>
 
-        <object data={ResumePDF} type="application/pdf" aria-label="Sean Welsh Brown Software Engineering Resume" style={{ height: `75vh`, width: `100%` }}></object>
-
-        <a href={ResumePDF} download="Sean Welsh Brown - Software Engineer - Resume">Download as a PDF</a>
-        
+        <a href={ResumePDF} download="Sean Welsh Brown - Software Engineer - Resume">
+          Download as a PDF
+        </a>
       </Content>
-
     </Layout>
   )
 }
